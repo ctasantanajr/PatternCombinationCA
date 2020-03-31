53f13a0e8b0fbe4a9b3b8894f955b51e057fad9f
ctasantanajr <33551129+ctasantanajr@users.noreply.github.com>
2020-03-31 19:05:32 +0100

    Modifying functionalities in Main

--- src/Main.java
+++ src/Main.java
@@ -41,19 +41,8 @@ public class Main {
 			System.out.println("Enter 4 to save a new country");
 			System.out.println("Enter 5 to quit without saving");
 
-			String answer = "";
-
 			// IN THIS POINT THE PROGRAM WILL VALIDATE THE OPTION ENTERED BY USER
-			try {
-				boolean valid = false;
-				do {
-					answer = sc.next();
-					valid = ValidateMenuOption(answer);
-				} while (valid == false);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("Error reading input");
-			}
+			String answer = ValidateMenuOption();
 
 			// this conditional switch executes blocks of methods in regards to the option
 			// typed by the user. After executing the whole block of commands it breaks and
@@ -72,28 +61,12 @@ public class Main {
 
 			case "3":
 
-				// GETTING ONLY THE COUNTRY THAT HAS THE GIVEN
-				// NAME
-				countries = db.findCountryByName("Test");
-
-				// PRINTING IT TO THE CONSOLE
-				for (Country c : countries) {
-					System.out.println(c);
-				}
+				FindCountryByName();
 				break;
 
 			case "4":
 
-				// CREATING A NEW COUNTRY. KEEP IN MIND THAT
-				// THE ID OF THE NEW COUNTRY IS GOING TO BE THE
-				// SIZE OF THE ARRAY PLUS ONE
-				Country newCountry = new Country("TXX", "Test", Continent.EUROPE, (float) 12345.87, "Heaven");
-
-				// ADDING THE COUNTRY TO THE ARRAY, TO HAVE LOCAL
-				// CONTROL OF THE DATA
-				countries.add(newCountry);
-				// ADDING THE NEW COUNTRY INTO THE DATABASE
-				System.out.println(db.saveCountry(newCountry));
+				AddCountry();
 				break;
 
 			case "5":
@@ -110,27 +83,41 @@ public class Main {
 	// METHOD TO VALIDATE THE OPTION ENTERED BY USER
 	// ------------------------------------------------------------------
 
-	private boolean ValidateMenuOption(String answer) {
+	private String ValidateMenuOption() {
 		// TODO Auto-generated method stub
 
 		boolean valid = false;
+		String answer = "";
 
-		if (answer.matches("[0-9]+")) {
-			if (Integer.parseInt(answer) == 1 || Integer.parseInt(answer) == 2 || Integer.parseInt(answer) == 3
-					|| Integer.parseInt(answer) == 4 || Integer.parseInt(answer) == 5) {
-				valid = true;
+		try {
 
-			} else {
-				valid = false;
-				System.out.println("Invalid option. Please try again!");
-			}
+			do {
 
-		} else {
+				answer = sc.next();
 
-			System.out.println("Please choose one of the options!");
+				if (answer.matches("[0-9]+")) {
+					if (Integer.parseInt(answer) == 1 || Integer.parseInt(answer) == 2 || Integer.parseInt(answer) == 3
+							|| Integer.parseInt(answer) == 4 || Integer.parseInt(answer) == 5) {
+						valid = true;
+
+					} else {
+						valid = false;
+						System.out.println("Invalid option. Please try again!");
+					}
+
+				} else {
+
+					System.out.println("Please choose one of the options!");
+				}
+
+			} while (valid == false);
+
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error reading menu option input");
 		}
 
-		return valid;
+		return answer;
 
 	}
 
@@ -179,35 +166,168 @@ public class Main {
 
 	private static void FindCountryByCode() {
 
-		String code = "";
 		System.out.println("Please inform the code: ");
 
+		String code = ValidateCode();
+
+		// GETTING ONLY THE COUNTRY THAT HAS THE GIVEN CODE
+		c = db.findCountryByCode(code);
+
+		// PRINTING IT TO THE CONSOLE
+		System.out.println(c);
+
+	}
+
+	// --------------------------------------------------------------------
+	// METHOD TO FIND A COUNTRY BY ITS NAME
+	// --------------------------------------------------------------------
+
+	private static void FindCountryByName() {
+
+		System.out.println("Please inform the name: ");
+		String name = sc.next();
+
+		// GETTING ONLY THE COUNTRY THAT HAS THE GIVEN NAME
+		countries = db.findCountryByName(name);
+
+		// PRINTING IT TO THE CONSOLE
+		for (Country c : countries) {
+			System.out.println(c);
+		}
+
+	}
+
+	// --------------------------------------------------------------------
+	// METHOD TO VALIDATE A CODE ENTERED BY USER
+	// --------------------------------------------------------------------
+
+	private static String ValidateCode() {
+
+		String code = "";
+
 		try {
-			
+
 			do {
 
 				code = sc.next();
 
-				if (code.length() == 3) {
-
-					// GETTING ONLY THE COUNTRY THAT HAS THE GIVEN CODE
-					c = db.findCountryByCode(code);
-
-					// PRINTING IT TO THE CONSOLE
-					System.out.println(c);
-
-				} else {
+				if (code.length() != 3) {
 
 					System.out.println("Please inform a code of 3 digits: ");
 
 				}
 
 			} while (code.length() != 3);
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			System.out.println("Problem finding a country by code");
+			System.out.println("Error reading input code");
 		}
+
+		return code;
+
+	}
+
+	// --------------------------------------------------------------------
+	// METHOD TO VALIDATE A CONTINENT ENTERED BY USER
+	// --------------------------------------------------------------------
+
+	private static Continent ValidateContinent() {
+
+		Continent continent = null;
+
+		try {
+
+			boolean valid = false;
+
+			do {
+
+				String option = sc.next();
+
+				if (option.matches("[0-9]+")) {
+					if (Integer.parseInt(option) == 1 || Integer.parseInt(option) == 2 || Integer.parseInt(option) == 3
+							|| Integer.parseInt(option) == 4 || Integer.parseInt(option) == 5
+							|| Integer.parseInt(option) == 6 || Integer.parseInt(option) == 7) {
+						valid = true;
+
+						switch (option) {
+
+						case "1":
+							continent = Continent.ASIA;
+							break;
+						case "2":
+							continent = Continent.EUROPE;
+							break;
+						case "3":
+							continent = Continent.NORTH_AMERICA;
+							break;
+						case "4":
+							continent = Continent.AFRICA;
+							break;
+						case "5":
+							continent = Continent.OCEANIA;
+							break;
+						case "6":
+							continent = Continent.ANTARCTICA;
+							break;
+						case "7":
+							continent = Continent.SOUTH_AMERICA;
+							break;
+
+						}
+
+					} else {
+						valid = false;
+						System.out.println("Invalid option");
+					}
+				}
+
+			} while (valid == false);
+
+		} catch (Exception e) {
+			System.out.println("Error reading input");
+		}
+
+		return continent;
+
+	}
+
+	// --------------------------------------------------------------------
+	// METHOD TO ADD A NEW COUNTRY
+	// --------------------------------------------------------------------
+
+	private static void AddCountry() {
+
+		System.out.println("Enter a code (3 characters): ");
+		String code = ValidateCode();
+
+		System.out.println("Enter a name: ");
+		String name = sc.next();
+
+		System.out.println("Choose a Continent:\n 1-Asia \n 2-Europe \n 3-North America \n 4-Africa "
+				+ "\n 5-Oceania \n 6-Antarctica \n 7-South America");
+		Continent continent = ValidateContinent();
+		
+		System.out.println("Enter a surface area: ");
+		float area = sc.nextFloat();
+		
+		System.out.println("Enter a head of state: ");
+		String state = sc.next();
+		
+		
+
+		//System.out.println(continent.getName());
+		
+		// CREATING A NEW COUNTRY. KEEP IN MIND THAT
+		// THE ID OF THE NEW COUNTRY IS GOING TO BE THE
+		// SIZE OF THE ARRAY PLUS ONE
+		Country newCountry = new Country("TXX", "Test", Continent.EUROPE, (float) 12345.87, "Heaven");
+
+		// ADDING THE COUNTRY TO THE ARRAY, TO HAVE LOCAL
+		// CONTROL OF THE DATA
+		countries.add(newCountry);
+		// ADDING THE NEW COUNTRY INTO THE DATABASE
+		System.out.println(db.saveCountry(newCountry));
 
 	}
 
